897c9e5931b6f583c884bfd7c92f18733a1a9baf
Daniel Volski <danfvdaum3@hotmail.com>
2022-08-01 22:02:58 -0300

    Refatorado e adicionado o método para imprimir o caminho do grafo

--- main.py
+++ main.py
@@ -9,30 +9,57 @@ import random
 import copy
 import time
 
-def cria_estado_aleatorio(pilhas):
-    blocos = ["A", "B", "C"]
-    while blocos:
-        pilhas[random.randint(0, 2)].push(blocos.pop())
+class Estado:
+    def __init__(self, pai=None):
+        self.pai = pai
+        self.pilhas = [Pilha(), Pilha(), Pilha()]
+    
+    def __str__(self) -> str:
+        return str(self.pilhas)
+    
+    def __repr__(self) -> str:
+        return str(self.pilhas)
 
-def cria_estado_obj(pilhas):
-    blocos = ["A", "B", "C"]
-    while blocos:
-        pilhas[1].push(blocos.pop())
+    def __eq__(self, __o: object) -> bool:
+        return self.pilhas == __o.pilhas
 
-def expande(pilhas):
-    vertices = []
-    for i in range(0, 3):
-        if pilhas[i].contador == 0:
-            continue
-        else:
-            temp1 = copy.deepcopy(pilhas)
-            bloco = temp1[i].pop()
-            for j in range(0, 3):
-                temp2 = copy.deepcopy(temp1)
-                if i != j:
-                    temp2[j].push(bloco)
-                    vertices.append(temp2)
-    return vertices
+    def cria_estado_aleatorio(self):
+        blocos = ["A", "B", "C"]
+        while blocos:
+            self.pilhas[random.randint(0, 2)].push(blocos.pop())
+
+    def cria_estado_obj(self):
+        blocos = ["A", "B", "C"]
+        while blocos:
+            self.pilhas[1].push(blocos.pop())
+
+    def expande(self):
+        vertices = []
+        for i in range(0, len(self.pilhas)):
+            # Se a pilha atual estiver vazia, o loop vai direto para a próxima iteração
+            if self.pilhas[i].contador == 0:
+                continue
+            else:
+                # É feito uma cópia temporária do estado atual 
+                temp1 = copy.deepcopy(self)
+                # É retirado bloco do topo da pilha 
+                bloco = temp1.pilhas[i].pop()
+                for j in range(0, len(self.pilhas)):
+                    # Se i for igual a j a pilha é a mesma da iteração do loop mais externo
+                    if i != j:
+                        temp2 = copy.deepcopy(temp1)
+                        temp2.pai = self
+                        temp2.pilhas[j].push(bloco)
+                        vertices.append(temp2)
+        return vertices
+
+    def caminho(self) -> list:
+        caminho = []
+        aux = self
+        while aux:
+            caminho.append(aux)
+            aux = aux.pai
+        return caminho[::-1]
 
 def busca_largura(raiz, objetivo):
     vertices = []
@@ -41,26 +68,27 @@ def busca_largura(raiz, objetivo):
     vertices.append(raiz)
     visitados.append(raiz)
 
-    #inicio = time.time()
+    # Enquanto houverem vértices a busca continua
     while vertices:
         vertice = vertices.pop()
-        vizinhos = expande(vertice)
+        vizinhos = vertice.expande()
+        # A cada expansão do vértice um nível é aumentado
         nivel = nivel + 1
         for vizinho in vizinhos:
             if vizinho == objetivo:
                 print("Resultado encontrado no nível " + str(nivel) + " da árvore!")
                 return vizinho
+            # Se o vizinho já foi visitado, ele não é adicionado a lista de adjascência
             elif vizinho not in visitados:
                 visitados.append(vizinho)
                 vertices.append(vizinho)
 
 if __name__ == "__main__":
-    estado_ini = [Pilha(), Pilha(), Pilha()]
-    estado_obj = [Pilha(), Pilha(), Pilha()]
-    estado_teste = [Pilha(), Pilha(), Pilha()]
+    estado_ini = Estado()
+    estado_obj = Estado()
 
-    cria_estado_aleatorio(estado_ini)
-    cria_estado_aleatorio(estado_obj)
+    estado_ini.cria_estado_aleatorio()
+    estado_obj.cria_estado_obj()
 
     print("Estado inicial: " + str(estado_ini))
     print("Estado objetivo:" + str(estado_obj))
@@ -68,5 +96,6 @@ if __name__ == "__main__":
     inicio = time.time()
     resultado = busca_largura(estado_ini, estado_obj)
     fim = time.time()
-    print("Resultado: " + str(resultado))
+
+    print("Resultado: " + str(resultado.caminho()))
     print("Tempo total: " + str(fim - inicio))
